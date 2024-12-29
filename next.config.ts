416e6edf4153177ca23a3a8006a46c35519ed7cc
MrZaxter <SymphonyOfFinality@gmail.com>
2024-12-29 19:44:10 +0300

    header & header menu update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Указывает на статический экспорт
-    trailingSlash: true, // Добавляет завершающий слэш к URL
+
     images: {
         unoptimized: true, // Полностью отключает оптимизацию изображений
     },
+    basePath: '/foodforkiss',
+    assetPrefix: '/foodforkiss/',
 };
 
 export default nextConfig;
