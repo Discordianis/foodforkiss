2f13fbe53865bfa4b43e679f7c7ec82deb68b9ea
MrZaxter <SymphonyOfFinality@gmail.com>
2024-12-29 18:14:20 +0300

    header & header menu update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+    // Указываем подкаталог, например, если ваш репозиторий называется `food-for-happiness`, то:
+    basePath: '/foodforkiss',
+    assetPrefix: '/foodforkiss/',
+
+    // Разрешаем использование статического экспорта
+    trailingSlash: true,
+};
+
 const nextConfig: NextConfig = {
     output: 'export',
 };
